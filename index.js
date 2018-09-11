6abd87be086011e177a5b4e86c3e08ab7842d143
Loree Nichols <loree.d.nichols@gmail.com>
2018-09-11 16:29:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ return allMusicians;
 }
 
 function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"];
+  
   var newFacts = [];
   var i = 0;
   while (i < 4) {
